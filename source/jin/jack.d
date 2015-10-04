d00b9a25b5ebd9ce6e55b9a52c51463ba870cff7
jin <nin-jin@ya.ru>
2015-10-04 23:38:38 +0300

    hack -> jack + summ + abs

--- source/jin/jack.d
+++ source/jin/jack.d
@@ -1,38 +1,41 @@
 module jin.jack;
 
 import std.stdio;
-import jin.tree;
 import std.file;
 import std.conv;
+import std.math;
+import std.algorithm;
+import std.array;
+import jin.tree;
 
 class Jack {
 
-	Tree hack( File code ) {
-		return this.hack( Tree.parse( code ) );
+	Tree jack( File code ) {
+		return this.jack( Tree.parse( code ) );
 	}
 
-	Tree hack( string code , string uri = "" ) {
-		return this.hack( Tree.parse( code , uri ) );
+	Tree jack( string code , string uri = "" ) {
+		return this.jack( Tree.parse( code , uri ) );
 	}
 
-	Tree hack( Tree code ) {
+	Tree jack( Tree code ) {
 		auto jacks = [
 			"test" : delegate( Tree node ) {
 				if( node.length != 2 ) {
 					throw new Exception( "Supports only two arguments" );
 				}
-				auto one = this.hack( node[0] );
-				auto two = this.hack( node[1] );
+				auto one = this.jack( node[0] );
+				auto two = this.jack( node[1] );
 				if( one.to!string != two.to!string ) {
-					throw new Exception( "Test fail" );
+					throw new Exception( "Test fail:\n" ~ one.to!string ~ two.to!string ~ "----------------" );
 				}
 				return one;
 			},
 			"log" : delegate( Tree node ) {
 				if( node.length != 1 ) {
-					throw new Exception( "Supports only one argument " );
+					throw new Exception( "Supports only one argument" );
 				}
-				auto res = this.hack( node[0] );
+				auto res = this.jack( node[0] );
 				res.pipe( stdout );
 				return res;
 			},
@@ -40,24 +43,37 @@ class Jack {
 				return node;
 			},
 			"jack" : delegate( Tree node ) {
-				return this.hack( Tree.List( node[0].childs ) );
+				return this.jack( Tree.List( node[0].childs ) );
 			},
 			"head" : delegate( Tree node ) {
 				if( node.length != 1 ) {
 					throw new Exception( "Supports only one argument" );
 				}
-				return this.hack( node[0] )[0];
+				return this.jack( node[0] )[0];
 			},
 			"tail" : delegate( Tree node ) {
 				if( node.length != 1 ) {
 					throw new Exception( "Supports only one argument" );
 				}
-				return this.hack( node[0] )[ $ - 1 ];
+				return this.jack( node[0] )[ $ - 1 ];
+			},
+			"summ" : delegate( Tree node ) {
+				int res = 0;
+				foreach( Tree child ; node.childs ) {
+					res += this.jack( child ).value!int;
+				}
+				return Tree.Name( "int" , [ Tree.Value( res.to!string ) ] );
+			},
+			"abs" : delegate( Tree node ) {
+				auto childs = node.childs.map!( ( child ) {
+					return Tree.Name( "int" , [ Tree.Value( abs( child.value!int ).to!string ) ] );
+				} ).array;
+				return Tree.List( childs );
 			},
 			"" : delegate( Tree node ) {
 				Tree[] childs = [];
 				foreach( Tree child ; node.childs ) {
-					auto res = this.hack( child );
+					auto res = this.jack( child );
 					if( res.name ) {
 						childs ~= res;
 					} else {
@@ -82,7 +98,7 @@ class Jack {
 }
 
 unittest {
-	auto jack = new Jack;
-	auto test = Tree.parse( File( "./examples/test.jack.tree" ) );
-	auto res = jack.hack( test );
+	auto base = new Jack;
+	auto res = base.jack( File( "./examples/test.jack.tree" ) );
+	res.pipe( stdout );
 }
