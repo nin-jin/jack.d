c23d0d90d3fecd81d814a38ab8663fb9fd2adb33
jin <nin-jin@ya.ru>
2015-10-05 15:05:28 +0300

    Jack -> Tool[string] simple numbers

--- source/jin/jack.d
+++ source/jin/jack.d
@@ -8,97 +8,119 @@ import std.algorithm;
 import std.array;
 import jin.tree;
 
-class Jack {
+struct Tool {
+	Tree function( Tool[ string ] , Tree ) handler;
+	alias handler this;
+}
+alias Tools = Tool[ string ];
 
-	Tree jack( File code ) {
-		return this.jack( Tree.parse( code ) );
-	}
-
-	Tree jack( string code , string uri = "" ) {
-		return this.jack( Tree.parse( code , uri ) );
-	}
-
-	Tree jack( Tree code ) {
-		auto jacks = [
-			"test" : delegate( Tree node ) {
-				if( node.length != 2 ) {
-					throw new Exception( "Supports only two arguments" );
-				}
-				auto one = this.jack( node[0] );
-				auto two = this.jack( node[1] );
-				if( one.to!string != two.to!string ) {
-					throw new Exception( "Test fail:\n" ~ one.to!string ~ two.to!string ~ "----------------" );
-				}
-				return one;
-			},
-			"log" : delegate( Tree node ) {
-				if( node.length != 1 ) {
-					throw new Exception( "Supports only one argument" );
-				}
-				auto res = this.jack( node[0] );
-				res.pipe( stdout );
-				return res;
-			},
-			"list" : delegate( Tree node ) {
-				return node;
-			},
-			"jack" : delegate( Tree node ) {
-				return this.jack( Tree.List( node[0].childs ) );
-			},
-			"head" : delegate( Tree node ) {
-				if( node.length != 1 ) {
-					throw new Exception( "Supports only one argument" );
-				}
-				return this.jack( node[0] )[0];
-			},
-			"tail" : delegate( Tree node ) {
-				if( node.length != 1 ) {
-					throw new Exception( "Supports only one argument" );
-				}
-				return this.jack( node[0] )[ $ - 1 ];
-			},
-			"summ" : delegate( Tree node ) {
-				int res = 0;
-				foreach( Tree child ; node.childs ) {
-					res += this.jack( child ).value!int;
-				}
-				return Tree.Name( "int" , [ Tree.Value( res.to!string ) ] );
-			},
-			"abs" : delegate( Tree node ) {
-				auto childs = node.childs.map!( ( child ) {
-					return Tree.Name( "int" , [ Tree.Value( abs( child.value!int ).to!string ) ] );
-				} ).array;
-				return Tree.List( childs );
-			},
-			"" : delegate( Tree node ) {
-				Tree[] childs = [];
-				foreach( Tree child ; node.childs ) {
-					auto res = this.jack( child );
-					if( res.name ) {
-						childs ~= res;
-					} else {
-						childs ~= res.childs;
-					}
-				}
-				return node.clone( childs );
-			},
-		];
-
-		try {
-			if( code.name in jacks ) {
-				return jacks[ code.name ]( code );
-			}
-			return jacks[ "" ]( code );
-		} catch( Throwable e ) {
-			e.msg ~= "\n" ~ code.uri;
-			throw e;
-		}
-	}
-
+Tree jack( Tools tools , File code ) {
+	return tools.jack( Tree.parse( code ) );
 }
 
+Tree jack( File code , Tools tools ) {
+	return tools.jack( Tree.parse( code ) );
+}
+
+Tree jack( Tools tools , string code , string uri = "" ) {
+	return tools.jack( Tree.parse( code , uri ) );
+}
+
+Tree jack( Tools tools , Tree code ) {
+	return code.jack( tools );
+}
+
+Tree jack( Tree code , Tools tools ) {
+	try {
+		if( code.name in tools ) {
+			return tools[ code.name ]( tools , code );
+		}
+		return tools[ "" ]( tools , code );
+	} catch( Throwable e ) {
+		e.msg ~= "\n" ~ code.uri;
+		throw e;
+	}
+}
+
+Tools hack( Tools base , Tree function( Tools , Tree ) [ string ] addon ) {
+	Tools tools;
+	foreach( name , handler ; addon ) {
+		tools[ name ] = Tool( addon[ name ] );
+	}
+	foreach( name , tool ; base ) {
+		if( name in addon ) continue;
+		tools[ name ] = tool;
+	}
+	return tools;
+}
+
+static Tools toolsEmpty = null;
+static this() { toolsEmpty = [
+	"" : Tool( ( Tools tools , Tree code ) {
+		Tree[] childs = [];
+		foreach( Tree child ; code.childs ) {
+			auto res = tools.jack( child );
+			if( res.name ) {
+				childs ~= res;
+			} else {
+				childs ~= res.childs;
+			}
+		}
+		return code.clone( childs );
+	} ),
+]; }
+
+static Tools toolsAll = null;
+static this() { toolsAll = toolsEmpty.hack([
+	"test" : ( Tools tools , Tree code ) {
+		if( code.length != 2 ) {
+			throw new Exception( "Supports only two arguments" );
+		}
+		auto one = tools.jack( code[0] );
+		auto two = tools.jack( code[1] );
+		if( one.to!string != two.to!string ) {
+			throw new Exception( "Test fail:\n" ~ one.to!string ~ two.to!string ~ "----------------" );
+		}
+		return one;
+	},
+	"log" : ( Tools tools , Tree code ) {
+		if( code.length != 1 ) {
+			throw new Exception( "Supports only one argument" );
+		}
+		auto res = tools.jack( code[0] );
+		res.pipe( stdout );
+		return res;
+	},
+	"list" : ( Tools tools , Tree code ) {
+		return Tree.List( code.childs );
+	},
+	"jack" : ( Tools tools , Tree code ) {
+		return tools.jack( Tree.List( code[0].childs ) );
+	},
+	"head" : ( Tools tools , Tree code ) {
+		if( code.length != 1 ) {
+			throw new Exception( "Supports only one argument" );
+		}
+		return tools.jack( code[0] )[0];
+	},
+	"tail" : ( Tools tools , Tree code ) {
+		if( code.length != 1 ) {
+			throw new Exception( "Supports only one argument" );
+		}
+		return tools.jack( code[0] )[ $ - 1 ];
+	},
+	"summ" : ( Tools tools , Tree code ) {
+		int res = 0;
+		foreach( Tree child ; code.childs ) {
+			res += tools.jack( child ).name.to!int;
+		}
+		return Tree.Name( res.to!string );
+	},
+	"abs" : ( Tools tools , Tree code ) {
+		return Tree.Name( abs( code[0].name.to!int ).to!string );
+	},
+]); }
+
 unittest {
-	auto base = new Jack;
-	auto res = base.jack( File( "./examples/test.jack.tree" ) );
-	res.pipe( stdout );
+	File( "./examples/test.jack.tree" ).jack( toolsAll ).pipe( stdout );
 }
